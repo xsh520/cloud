634aa359748a3ab3ac3d81b35121e9e0b2ecdc99
xsh520 <954186642@qq.com>
2020-06-21 22:09:15 +0800

    1

---
+++ cloud-consumer-feign-order80/src/main/java/com/xsh/cloud/controller/PaymentFeignController.java
@@ -0,0 +1,27 @@
+package com.xsh.cloud.controller;
+
+import com.xsh.cloud.entities.CommonResult;
+import com.xsh.cloud.entities.Payment;
+import com.xsh.cloud.service.PaymentFeignService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class PaymentFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping(value = "/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
+        return paymentFeignService.getPaymentById(id);
+    }
+
+    @GetMapping(value = "/consumer/payment/feign/timeout")
+    public String paymentFeignTimeout() {
+        return paymentFeignService.paymentFeignTimeout();
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/xsh/cloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/xsh/cloud/controller/PaymentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @auther zzyy
@@ -77,5 +78,14 @@ public class PaymentController
         return serverPort;
     }
 
+    @GetMapping(value = "/payment/feign/timeout")
+    public String paymentFeignTimeout() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return serverPort;
+    }
 
 }
